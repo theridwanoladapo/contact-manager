c28bab21c0f0e8f13c2e81d661d055014cf4e970
Giwa Wahab Tomiwa <giwawahabtomiwa2022@gmail.com>
2025-07-05 14:28:50 +0100

    Update route list

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(function () {
+Route::group([], function () {
     Route::get('/contacts', [ContactController::class, 'index']);
     Route::post('/contacts', [ContactController::class, 'store']);
 });
